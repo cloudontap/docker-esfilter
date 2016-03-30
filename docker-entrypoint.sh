7f92050c4754318fda9c59e56deecc15ff283f5b
Michael Leditschke <mike@ammd.com.au>
2016-03-30 22:49:23 +1100

    Place generation of nginx configuration in a separate script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 
-# Stuff that should already be in the image
-GOSOURCE_STARTUP=/opt/gosource/startup
-GOSOURCE_ESFILTER=/opt/gosource/startup/esfilter
-
-# Location to put config files
-NGINX_CONFIG=/etc/nginx
-
-# Save the configuration to a file
-CONFIG_FILE=/tmp/config.json
-echo ${CONFIGURATION} > ${CONFIG_FILE}
-
-# Create the ip lists
-for LIST in data query; do
-	# Generate the logstash configuration file
-	VARIABLES=""
-	VARIABLES="${VARIABLES} -v configuration=${CONFIG_FILE}"
-	VARIABLES="${VARIABLES} -v list=${LIST}"
-	
-	# Generate the logstash configuration for loading data to elasticsearch
-	java -jar ${GOSOURCE_STARTUP}/gsgen.jar -i ip_list.ftl -d ${GOSOURCE_ESFILTER} -o ${NGINX_CONFIG}/ip_${LIST}.conf $VARIABLES
-done
-
-# Now the top level nginx configuration
-VARIABLES=""
-VARIABLES="${VARIABLES} -v CONFIGURATION=${CONFIG_FILE}"
-
-java -jar ${GOSOURCE_STARTUP}/gsgen.jar -i nginx.ftl -d ${GOSOURCE_ESFILTER} -o ${NGINX_CONFIG}/nginx.conf $VARIABLES
+# Generate the necessary configuration
+/opt/gosource/startup/esfilter.sh
 
 #this line starts nginx daemon in foreground and should be the last one
 /usr/sbin/nginx -g "daemon off;"
