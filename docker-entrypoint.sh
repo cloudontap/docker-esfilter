ae67b5e4476e897459113e80278fe32cd1338afe
Michael Leditschke <mike@ammd.com.au>
2016-04-17 19:22:47 +1000

    Dummy commit to test tagging in dockerhub

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@
 htpasswd -bc /etc/nginx/.htpasswd $DATA_USERNAME  $DATA_PASSWORD 
 htpasswd -b  /etc/nginx/.htpasswd $QUERY_USERNAME $QUERY_PASSWORD
 
-# Generate the necessary configuration
+# Generate the necessary configuration 
 /opt/gosource/startup/esfilter.sh
 
 #this line starts nginx daemon in foreground and should be the last one
