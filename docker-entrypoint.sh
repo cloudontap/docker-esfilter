2d0e7190a3499cf6fe2aab0bdbbed9adcc31348c
Michael Leditschke <mike@ammd.com.au>
2016-06-01 13:10:05 +1000

    Re-enable esfilter setup script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ htpasswd -bc /etc/nginx/.htpasswd $DATA_USERNAME  $DATA_PASSWORD
 htpasswd -b  /etc/nginx/.htpasswd $QUERY_USERNAME $QUERY_PASSWORD
 
 # Generate the necessary configuration 
-#/opt/gosource/startup/esfilter.sh
+/opt/gosource/startup/esfilter.sh
 
 #this line starts nginx daemon in foreground and should be the last one
 /usr/sbin/nginx -g "daemon off;"
